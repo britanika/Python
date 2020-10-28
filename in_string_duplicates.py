9e5ff00045be6f8394c183668f0bd7960c17557f
britanika <sas4.ciric@gmail.com>
2020-10-28 14:37:36 +0100

    Solution two

---
+++ in_string_duplicates.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+letters = input("Write the string: ")
+cnt = Counter(letters)
+
+for key, value in cnt.items():
+    if value > 1 and key != " ":
+        print(key, value)
