dba9cd4ff22c008bc036ca98410a845965000167
britanika <sas4.ciric@gmail.com>
2020-10-28 14:27:24 +0100

    Solution One

---
+++ duplicates_in_string.py
@@ -0,0 +1,12 @@
+letters = input("Write the string: ")
+duplicates = []
+for i in range(0, len(letters)):
+    counter = 1
+    for j in range(i + 1, len(letters)):
+        if letters[i] == letters[j] and letters[i] != ' ':
+            counter = counter + 1
+    if counter > 1 and letters[i] not in duplicates:
+        duplicates.append(letters[i])
+
+
+print(f'Duplicates in string are: {duplicates}')
