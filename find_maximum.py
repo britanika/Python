a054350940b3e019a146cd77d232a603b5d32bfc
sasa <sas4.ciric@gmail.com>
2023-08-28 17:38:17 +0200

    Find maximum in the list

--- find_maximum.py
+++ find_maximum.py
@@ -1,11 +1,12 @@
-def find_max(numbers):
+def find_max(numbers: list) -> int:
     maximum = numbers[0]
     for number in numbers:
         if number > maximum:
             maximum = number
-    print(maximum)
+    return maximum
 
 
 num = [3, 5, 2, 7, 8]
 find_max(num)
+# Output: 8
 
